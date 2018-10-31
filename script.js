3eaea271ddabccea67cf881a453741a2fd3d1532
christian <detmig007@hotmail.com>
2018-10-31 19:30:50 +0100

    små fejl

--- script.js
+++ script.js
@@ -7,14 +7,17 @@ function sidenVises() {
 }
 
 function showStart() {
-    console.log("ShowVises");
+    console.log("ShowStart");
+    document.querySelector("#start").classList.remove("hide");
+    document.querySelector("#start").classList.remove("menu_just_do_it");
     // Vis Startskærm
     document.querySelector("#start").classList.add("show");
     // start animation på start-knap
     document.querySelector("#play").classList.add("pulse");
 
-
-
+    document.querySelector("#rotte").classList.remove("paused");
+    document.querySelector("#slimepoop").classList.remove("paused");
+    document.querySelector("#poo").classList.remove("paused");
 
 }
 
@@ -39,13 +42,14 @@ function startGame() {
     // skjul startskærm
     document.querySelector("#start").classList.add("hide");
     //Vis spilskærm
-    document.querySelector("#play").classList.add("show");
+    document.querySelector("#screen").classList.add("show");
     // poo falder
     document.querySelector("#poo").classList.add("falling");
     //slimepoo falder
     document.querySelector("#slimepoop").classList.add("falling");
     //rat går igang
     document.querySelector("#rotte").classList.add("rat_race");
+    document.querySelector("#rotte").classList.remove("hide");
 
 }
 document.querySelector("#rotte").addEventListener("click", ratForsvind);
@@ -53,11 +57,13 @@ document.querySelector("#rotte").addEventListener("click", ratForsvind);
 function ratForsvind() {
 
     console.log("ratForsvind");
-
+    //rotte på pause
     document.querySelector("#rotte").classList.add("paused");
+    //rotte forsvinder
     document.querySelector("#rotte").classList.add("hide");
 
 }
+// klik på poo
 document.querySelector("#poo").addEventListener("click", pooForsvind);
 
 function pooForsvind() {
@@ -68,6 +74,7 @@ function pooForsvind() {
     document.querySelector("#poo").classList.add("dissappear");
 
 }
+// klik på slime
 document.querySelector("#slimepoop ").addEventListener("click", slimForsvind);
 
 function slimForsvind() {
@@ -90,13 +97,52 @@ function showSettings() {
 
 }
 //klik "X"
-document.querySelector("#exit").addEventListener("click", hideSettings);
+document.querySelector("#exit , #exit2, #exit3").addEventListener("click", hideSettings);
 
 function hideSettings() {
     console.log("hideSettings");
     // fjern settings menu
     document.querySelector("#settings").classList.add("hide");
+    // fjern settings menu
+    document.querySelector("#gameOver").classList.add("hide");
+    // fjern settings menu
+    document.querySelector("#levelComplete").classList.add("hide");
     // fjern blur på titelscreen'
     document.querySelector("#start").classList.remove("blur");
+    // fjern menu gameOver
+    document.querySelector("#gameOver").classList.add("hide");
+    // fjern blur på game_background
+    document.querySelector("#game_background").classList.remove("blur");
 
+}
+// BARE INDTIL VIDERE ----------------------------------------------------
+document.querySelector("#points").addEventListener("click", gameOver);
+//------------------------------------------------------------------------
+function gameOver() {
+    console.log("gameOver")
+
+    document.querySelector("#gameOver").classList.remove("hide");
+    document.querySelector("#game_background").classList.add("blur");
+    document.querySelector("#rotte").classList.add("paused");
+    document.querySelector("#slimepoop").classList.add("paused");
+    document.querySelector("#poo").classList.add("paused");
+
+}
+document.querySelector("#restart").addEventListener("click", restartGame);
+
+function restartGame() {
+    console.log("restartGame");
+
+    document.querySelector("#gameOver").classList.add("hide");
+    document.querySelector("#game_background").classList.remove("blur");
+
+    showStart();
+}
+// BARE INDTIL VIDERE ----------------------------------------------------
+document.querySelector("#energy").addEventListener("click", levelComplete);
+//------------------------------------------------------------------------
+function levelComplete() {
+    console.log("levelComplete");
+    document.querySelector("#levelComplete").classList.remove("hide");
+    document.querySelector("#levelComplete").classList.add("show");
 }
